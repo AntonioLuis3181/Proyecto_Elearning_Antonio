e4d4eb042ccb6a360257f27c20ac1ebfd8e78f09
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2026-02-10 15:04:34 +0100

    Despliegue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Importamos el plugin
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // Activamos el plugin aquí
     nodePolyfills()
   ],
   base:'./',
